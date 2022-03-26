abd155634c04bb3d08b433fa3dbd7e7c50ff7d2e
Rodrigo farad <65503961+rodrigo-farad@users.noreply.github.com>
2022-03-26 13:36:10 -0300

    Update configuracao.php

--- Biblioteca/classes/configuracao.php
+++ Biblioteca/classes/configuracao.php
@@ -1,16 +1,16 @@
 <?php
 define('S',dirname(dirname(__FILE__)));
-define('DIRETORIO','/otica');   // diretório raiz do app
-define('NOME_APP','OpticalSystem');   //nome do app
-define('VERSAO','version 5.0');  //versão do app
-define('TELEFONE','(11)4118-267');  //versão do app
-define('SUPORTE','suporte@emitafacil.com.br');  //email suporte 
-define('SITE','meupagamento.bankpoupe.com.br');  //email suporte 
-define('CNPJ','38.2021.925/0001-73');  //email suporte 
+define('DIRETORIO','/nomeDir');   // diretório raiz do app
+define('NOME_APP','NomeAPP');   //nome do app
+define('VERSAO','VersãoAPP');  //versão do app
+define('TELEFONE','NUMEROtelefone');  //versão do app
+define('SUPORTE','EMail');  //email suporte 
+define('SITE','SITE');  //email suporte 
+define('CNPJ','CNPJ');  //CNPJ da empresa
 define('FUSO_HORARIO','America/Campo_Grande');  //fuso horário
 date_default_timezone_set('America/Campo_Grande');
 define('BANCO', [
-    'BANCO_NOME' => 'otica',
+    'BANCO_NOME' => 'BANCO',
     'HOST' => 'localhost',
     'USER' => 'root',
     'PASSWORD' => ''
